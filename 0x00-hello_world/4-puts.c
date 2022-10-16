532d9daf14b502685617c1436f3457d3515d9205
Biruktawit Haile <biruktiha@gmail.com>
2022-10-16 12:34:59 -0700

    First c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,11 @@
-/*                                                                                                                                      
-
- * main - print a line of code using puts function                                                                                      
-
- *                                                                                                                                      
-
- * Return: 0                                                                                                                            
-
-*/                                                                                                                                      
-
-int main (void)                                                                                                                         
-
-{  
-	puts("Â\"Programming is like building a multilingual puzzle");
-	return (0);
+#include <stdio.h>
+/*
+*main - print a line of code using puts function
+*
+*Return: 0
+*/
+int main (void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
