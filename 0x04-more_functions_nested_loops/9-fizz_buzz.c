112460861a5e42396e160be716f9e648f2a6e390
Biruktawit Haile <biruktiha@gmail.com>
2022-10-20 15:37:08 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 /**
-*main - prints fizzbuzz
+*main - prints frizzbuzz
 *Return: 0
 */
 int main(void)
 {
 int n;
-printf("1");
-for (n = 0; n <= 100; n++)
-[
-clear
 
+printf("1");
+for (n = 2; n <= 100; n++)
+{
+printf(" ");
+if (n % 3 == 0)
+printf("Fizzy");
+
+if (n % 5 == 0)
+printf("Buzz");
+
+if (n % 3 != 0 && n % 5 != 0)
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
