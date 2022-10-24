a249c2e6e3896e4b899d027ae67df5a2176fe8f6
Biruktawit Haile <biruktiha@gmail.com>
2022-10-24 14:34:47 -0700

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_strcpy - copies a string from source to destination
+*@dest: destination
+*@src: source
+*Return: string
+*/
+char *_strcpy(char *dest, char *src)
+{
+int len = 0;
+while (*(src + len) != '\0')
+{
+*(des + len) = *(src + len);
+len++;
+}
+*(des + len) = '\0';
+return (dest);
+}
