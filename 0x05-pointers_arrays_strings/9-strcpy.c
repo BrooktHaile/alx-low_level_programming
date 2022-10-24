adbc7219930c12e6f40500f9a0c308936edf5519
Biruktawit Haile <biruktiha@gmail.com>
2022-10-24 14:47:10 -0700

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,4 +10,9 @@ char *_strcpy(char *dest, char *src)
 int len = 0;
 while (*(src + len) != '\0')
 {
-*(des + len) = *(src + len);
+*(dest + len) = *(src + len);
+len++;
+}
+*(dest + len) = '\0';
+return (dest);
+}
