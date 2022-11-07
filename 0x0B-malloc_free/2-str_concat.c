604ab98bdbf83c7108e2e6b3f4f2b7fc894dff08
BrooktHaile <113849234+BrooktHaile@users.noreply.github.com>
2022-11-07 20:45:30 +0300

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,30 +1,24 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "bootcamp.h"
+#include <limits.h>
 /**
- * *_calloc - function to allocates memory
- * @nmemb: unsigned int type
- * @size: unsigned int type
- * Return: return pointer to array
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: elements of memory needed
+ * @size: size in bytes of each element
+ * Return: pointer to the allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ptr;
-	unsigned int count;
+	unsigned int i;
+	char *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
-	{
+	if (size >= UINT_MAX / nmemb || nmemb >= UINT_MAX / size)
 		return (NULL);
-	}
-	count = 0;
-	while (count < nmemb * size)
-	{
-		ptr[count] = 0;
-		count++;
-	}
-	return (ptr);
+	p = malloc(size * nmemb);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		p[i] = 0;
+	return ((void *)p);
 }
